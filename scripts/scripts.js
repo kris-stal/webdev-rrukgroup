9f093a6868727be8e63b8090d4011b39c983b799
Kris Stalmokas <pg011732@student.reading.ac.uk>
2025-10-01 19:02:00 +0100

    header fixed, logo fixed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,22 @@
+// Listeners
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    const rrlogo = document.querySelector('.nav-logo');
+    if (window.scrollY > 10) {
+        header.classList.add('scrolled');
+        rrlogo.src = "images/rrlogo.png";
+    } else {
+        header.classList.remove('scrolled');
+        rrlogo.src = "images/rrlogo2.png";
+    }
+});
+
+
+// Functions
 function getCurrentPage(){
     const currentPath = window.location.pathname;
     const pageNames = ["/pages", "/index",];
     let length = pageNames.length;
-
     let lengths = document.querySelectorAll ('li a')
     
     for (let i = 0; i < lengths.length; i++)
