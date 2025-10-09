291f0189abdb72afdd3a710549a1df57ff71780a
Kris Stalmokas <pg011732@student.reading.ac.uk>
2025-10-09 23:44:21 +0100

    laptop upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -20,7 +20,6 @@ window.addEventListener('scroll', function () {
 function getCurrentPage(){
     const currentPath = window.location.pathname;
     const pageNames = ["/pages", "/index",];
-    let length = pageNames.length;
     let lengths = document.querySelectorAll ('li a')
     
     for (let i = 0; i < lengths.length; i++)
